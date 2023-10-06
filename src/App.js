a611c74dba4c4d306fe8dd75d6f2de36323750cc
Janeibeh <janewhite.ji@gmail.com>
2023-10-06 14:38:55 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+import './App.css';
+import PlayerList from "./Component/PlayersList"
+
+
+
+function App() {
+
+return (
+
+  <div className="App">
+    <PlayerList/>
+  </div>
+
+);
+}
+
+export default App;
---
+++ src/players.js
@@ -0,0 +1,41 @@
+const playerData = [
+    {
+      id: "1",
+      name: "Lionel Messi",
+      team: "Argentina",
+      nationality: "Rosario",
+      jerseyNumber: "10",
+      age: "36 years",
+      image: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg"
+    },
+    {
+      id: "1",
+      name: "Christiano Ronaldo",
+      team: "Al-Nassr",
+      nationality: "Portugese",
+      jerseyNumber: "7",
+      age: "38 years",
+      image:"https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt882b82ad040c2e33/6516f31b436e363613ba5727/GettyImages-1696064005.jpg?auto=webp&format=pjpg&width=2048&quality=60"
+    },
+    {
+      id: "1",
+      name: "Zlatan Ibrahimovic",
+      team: "AC Milan",
+      nationality: "Sweden",
+      jerseyNumber: "10",
+      age: "42 years",
+      image: "https://i.guim.co.uk/img/media/f6dbb2a099c5400842157a4d54ea80e1ba871c13/0_76_2760_1656/master/2760.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=afaddec9db79b5833c86c45881eb32e6"
+    },
+    {
+      id: "1",
+      name: "Yaya Toure",
+      team: "Standard de Liège",
+      nationality: "Cote d'Ivoire",
+      jerseyNumber: "",
+      age: "40 years",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqKADWR218ViMBcpjQ5XC9lAmUTMQ5rUScQg&usqp=CAU"
+    },
+  ];
+  
+  export default playerData;
+  
